dc3230dc7dac43ab4e90cf537c29326d91d09fe7
gustavodiniz <gustavodinniz@hotmail.com>
2023-07-23 13:17:43 -0300

    feat: create QuotationEntity and QuotationRepository

---
+++ ms-report/src/main/java/br/com/mining/company/entity/QuotationEntity.java
@@ -0,0 +1,28 @@
+package br.com.mining.company.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "quotation_report")
+public class QuotationEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Date date;
+
+    @Column(name = "currency_price")
+    private BigDecimal currencyPrice;
+}
